d995b51d3b0de9278a9f5b5cd395ac848c7e7bd3
Vo Duc Phuoc <phuocnaldo92@gmail.com>
2017-04-10 16:50:04 +0700

    chapter8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by id: params[:id]
-    if @user
+    if @user.nil?
       render :error
     end
   end
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = I18n.t("wellcome2")
       redirect_to @user
     else
