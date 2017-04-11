59455a55006972e7b84bcbaff97e3d114cc208ba
Vo Duc Phuoc <phuocnaldo92@gmail.com>
2017-04-11 14:01:31 +0700

    chapter10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   
+  before_action :logged_in_user, except: [:new, :create, :show]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :find_user, except: [:index, :new, :create]
+
   def show
-    @user = User.find_by id: params[:id]
-    if @user.nil?
-      render :error
-    end
+  end
+
+  def index
+    @users = User.paginate page: params[:page]
   end
 
   def new
@@ -14,16 +19,59 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = I18n.t("wellcome2")
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = I18n.t "please_check"
+      redirect_to root_url
     else
       render :new
     end
   end
+
+  def destroy
+    @user.destroy
+    flash[:success] = I18n.t("user_deleted")
+    redirect_to users_url
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = I18n.t("profile_updated")
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = I18n.t("please_log_in")
+      redirect_to login_url
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      render :error
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
 	
-  attr_accessor :remember_token
-  before_save {email.downcase!}
-  validates :name,  presence: true, length: {maximum: Settings.max_num}
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.max_email},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.min_num}
+  validates :password, presence: true, length: {minimum: Settings.min_num}, allow_nil: true
   has_secure_password
 
   def User.digest string
@@ -31,5 +31,22 @@ class User < ApplicationRecord
 
   def forget
     update_attribute :remember_digest, nil
+  end
+
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
